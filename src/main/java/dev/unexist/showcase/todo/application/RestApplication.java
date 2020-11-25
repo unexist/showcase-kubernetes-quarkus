0b9e97f98607e591511183b0af00b95a5188b3a3
Christoph Kappel <christoph@unexist.dev>
2020-11-25 20:36:23 +0100

    Replaced annotation based OAS config with values from application.properties

--- src/main/java/dev/unexist/showcase/todo/application/RestApplication.java
+++ src/main/java/dev/unexist/showcase/todo/application/RestApplication.java
@@ -11,31 +11,7 @@
 
 package dev.unexist.showcase.todo.application;
 
-import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-import org.eclipse.microprofile.openapi.annotations.info.Contact;
-import org.eclipse.microprofile.openapi.annotations.info.Info;
-import org.eclipse.microprofile.openapi.annotations.info.License;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
-
 import javax.ws.rs.core.Application;
 
-@OpenAPIDefinition(
-    tags = {
-        @Tag(name = "Todo", description = "Simple todo service"),
-    },
-    info = @Info(
-        title = "Todo Service",
-        version = "0.7",
-        contact = @Contact(
-            name = "Christoph Kappel",
-            url = "https://unexist.dev",
-            email = "christoph@unexist.dev"
-        ),
-        license = @License(
-            name = "GPLv2",
-            url = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"
-        )
-    )
-)
 public class RestApplication extends Application {
 }
